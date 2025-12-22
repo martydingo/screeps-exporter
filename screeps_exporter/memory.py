b1366927ca91a549cac1e0f3ed6618a28c3c3fce
Martin George <marty@dingopaws.net>
2025-12-22 17:25:31 +1100

    Added Controller Metrics

--- screeps_exporter/memory.py
+++ screeps_exporter/memory.py
@@ -1,4 +1,4 @@
-from prometheus_client import Summary, start_http_server
+from prometheus_client import Gauge, Enum, start_http_server
 from time import sleep
 
 
@@ -38,17 +38,18 @@ class Memory:
     def createRoomMetrics(self):
         self.createRoomEnergyMetrics()
         self.createRoomSourceMetrics()
+        self.createRoomControllerMetrics()
 
     def createRoomEnergyMetrics(self):
         self.metrics["energy"] = {}
 
-        self.metrics["energy"]["amount"] = Summary(
+        self.metrics["energy"]["amount"] = Gauge(
             "screeps_room_energy_amount",
             documentation="The amount of energy in a room for use by spawners and extensions",
             labelnames=["room"],
         )
 
-        self.metrics["energy"]["capacity"] = Summary(
+        self.metrics["energy"]["capacity"] = Gauge(
             "screeps_room_energy_capacity",
             documentation="The total capacity of energy in a room for use by spawners and extensions",
             labelnames=["room"],
@@ -65,17 +66,17 @@ class Memory:
 
     def createRoomSourceMetrics(self):
         self.metrics["sources"] = {}
-        self.metrics["sources"]["amount"] = Summary(
+        self.metrics["sources"]["amount"] = Gauge(
             "screeps_source_amount",
             documentation="The amount of energy remaining for a given energy source",
             labelnames=["room", "source"],
         )
-        self.metrics["sources"]["capacity"] = Summary(
+        self.metrics["sources"]["capacity"] = Gauge(
             "screeps_source_capacity",
             documentation="The total capacity of energy for a given energy source",
             labelnames=["room", "source"],
         )
-        self.metrics["sources"]["regeneration"] = Summary(
+        self.metrics["sources"]["regeneration"] = Gauge(
             "screeps_source_regeneration",
             documentation="The regeneration timer for a given energy source to replete",
             labelnames=["room", "source"],
@@ -98,6 +99,78 @@ class Memory:
             except KeyError:
                 pass
 
+    def createRoomControllerMetrics(self):
+        self.metrics["controllers"] = {}
+
+        self.metrics["controllers"]["upgrade"] = {}
+        self.metrics["controllers"]["upgrade"]["progress"] = Gauge(
+            "screeps_controllers_upgrade_progress",
+            documentation="The amount of energy invested in the room controller to upgrade the room control level",
+            labelnames=["room", "controller"],
+        )
+        self.metrics["controllers"]["upgrade"]["nextLevel"] = Gauge(
+            "screeps_controllers_upgrade_next_level",
+            documentation="The amount of energy required to upgrade the room control level to the next level",
+            labelnames=["room", "controller"],
+        )
+
+        self.metrics["controllers"]["safeMode"] = {}
+        self.metrics["controllers"]["safeMode"]["active"] = Enum(
+            "screeps_controllers_safe_mode_active",
+            documentation="Whether safe mode is currently active for a given room",
+            labelnames=["room", "controller"],
+            states=["active", "inactive"],
+        )
+        self.metrics["controllers"]["safeMode"]["available"] = Gauge(
+            "screeps_controllers_safe_mode_next_available",
+            documentation="How many safe modes currently remain for a given room",
+            labelnames=["room", "controller"],
+        )
+        self.metrics["controllers"]["safeMode"]["timeLeft"] = Gauge(
+            "screeps_controllers_safe_mode_time_left",
+            documentation="The duration of time before safe mode deactivates for a given room",
+            labelnames=["room", "controller"],
+        )
+        self.metrics["controllers"]["downgrade"] = Gauge(
+            "screeps_controllers_downgrade",
+            documentation="The amount of ticks remaining before the room level decrements",
+            labelnames=["room", "controller"],
+        )
+        self.metrics["controllers"]["level"] = Gauge(
+            "screeps_controllers_level",
+            documentation="The current room control level",
+            labelnames=["room", "controller"],
+        )
+
+        for roomName in self.roomMemory:
+            try:
+                controllerData = self.roomMemory[roomName]["controller"]
+                for controllerId in controllerData:
+                    self.metrics["controllers"]["upgrade"]["progress"].labels(
+                        room=roomName, controller=controllerId
+                    )
+                    self.metrics["controllers"]["upgrade"]["nextLevel"].labels(
+                        room=roomName, controller=controllerId
+                    )
+                    self.metrics["controllers"]["safeMode"]["active"].labels(
+                        room=roomName, controller=controllerId
+                    )
+                    self.metrics["controllers"]["safeMode"]["available"].labels(
+                        room=roomName, controller=controllerId
+                    )
+                    self.metrics["controllers"]["safeMode"]["timeLeft"].labels(
+                        room=roomName, controller=controllerId
+                    )
+                    self.metrics["controllers"]["downgrade"].labels(
+                        room=roomName, controller=controllerId
+                    )
+                    self.metrics["controllers"]["level"].labels(
+                        room=roomName, controller=controllerId
+                    )
+
+            except KeyError:
+                pass
+
     def pollMetrics(self):
         memory = self.pollMemory()
 
@@ -113,16 +186,17 @@ class Memory:
     def pollRoomMetrics(self):
         self.pollRoomEnergyMetrics()
         self.pollRoomSourceMetrics()
+        self.pollRoomControllerMetrics()
 
     def pollRoomEnergyMetrics(self):
         for roomName in self.roomMemory:
             try:
                 energyData = self.roomMemory[roomName]["energy"]
-                self.metrics["energy"]["amount"].labels(room=roomName).observe(
+                self.metrics["energy"]["amount"].labels(room=roomName).set(
                     energyData["amount"]
                 )
 
-                self.metrics["energy"]["capacity"].labels(room=roomName).observe(
+                self.metrics["energy"]["capacity"].labels(room=roomName).set(
                     energyData["capacity"]
                 )
 
@@ -137,13 +211,61 @@ class Memory:
                 for sourceId in sourceData:
                     self.metrics["sources"]["amount"].labels(
                         room=roomName, source=sourceId
-                    ).observe(sourceData[sourceId]["energy"]["amount"])
+                    ).set(sourceData[sourceId]["energy"]["amount"])
                     self.metrics["sources"]["capacity"].labels(
                         room=roomName, source=sourceId
-                    ).observe(sourceData[sourceId]["energy"]["capacity"])
+                    ).set(sourceData[sourceId]["energy"]["capacity"])
                     self.metrics["sources"]["regeneration"].labels(
                         room=roomName, source=sourceId
-                    ).observe(sourceData[sourceId]["regeneration"])
+                    ).set(sourceData[sourceId]["regeneration"])
 
             except KeyError:
                 pass
+
+    def pollRoomControllerMetrics(self):
+        for roomName in self.roomMemory:
+            try:
+                controllerData = self.roomMemory[roomName]["controller"]
+                for controllerId in controllerData:
+                    self.metrics["controllers"]["upgrade"]["progress"].labels(
+                        room=roomName, controller=controllerId
+                    ).set(controllerData[controllerId]["upgrade"]["progress"])
+
+                    self.metrics["controllers"]["upgrade"]["nextLevel"].labels(
+                        room=roomName, controller=controllerId
+                    ).set(controllerData[controllerId]["upgrade"]["nextLevel"])
+
+                    if controllerData[controllerId]["safeMode"]["active"] == True:
+                        safeModeState = "active"
+                    else:
+                        safeModeState = "inactive"
+
+                    self.metrics["controllers"]["safeMode"]["active"].labels(
+                        room=roomName, controller=controllerId
+                    ).state(safeModeState)
+
+                    self.metrics["controllers"]["safeMode"]["available"].labels(
+                        room=roomName, controller=controllerId
+                    ).set(controllerData[controllerId]["safeMode"]["available"])
+
+                    try:
+                        self.metrics["controllers"]["safeMode"]["timeLeft"].labels(
+                            room=roomName, controller=controllerId
+                        ).set(controllerData[controllerId]["safeMode"]["timeLeft"])
+                    except KeyError as error:
+                        print(error)
+                        print(controllerData[controllerId]["safeMode"].keys())
+                        self.metrics["controllers"]["safeMode"]["timeLeft"].labels(
+                            room=roomName, controller=controllerId
+                        ).set(0)
+
+                    self.metrics["controllers"]["downgrade"].labels(
+                        room=roomName, controller=controllerId
+                    ).set(controllerData[controllerId]["downgrade"])
+
+                    self.metrics["controllers"]["level"].labels(
+                        room=roomName, controller=controllerId
+                    ).set(controllerData[controllerId]["level"])
+
+            except KeyError as error:
+                print(error)
