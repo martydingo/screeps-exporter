1eb1b42ee71d487faaaaa3e6c272813177237ab3
Martin George <marty@dingopaws.net>
2025-12-26 18:49:50 +1100

    Added Syslog for Logging

--- screeps_exporter/syslog.py
+++ screeps_exporter/syslog.py
@@ -1,7 +1,6 @@
 from screepsapi import Socket
-import logging, logging.handlers
 from rfc5424logging import Rfc5424SysLogHandler
-import sys
+import logging, re, json
 
 DEFAULT_SHARD = "shard3"
 
@@ -22,13 +21,54 @@ class Syslog(Socket):
         self.log = logging.getLogger("screeps-syslog")
         self.log.addHandler(
             Rfc5424SysLogHandler(
-                address=(config["syslog"]["address"], config["syslog"]["port"])
+                address=(config["syslog"]["address"], config["syslog"]["port"]),
+                appname="screeps-syslog",
             )
         )
         self.log.setLevel(logging.DEBUG)
 
     def process_log(self, ws, message, shard=None):
-        self.log.info(message)
+        logSeverities = {
+            0: "emergency",
+            1: "alert",
+            2: "critical",
+            3: "error",
+            4: "warning",
+            5: "notice",
+            6: "informational",
+            7: "debug",
+        }
+        regexSearch = re.search(
+            '<font color="(.*)" severity="(.*) module=(.*)">.* - (.*)</font>', message
+        ).groups()
+        color = regexSearch[0]
+        severity = logSeverities[int(regexSearch[1])]
+        module = regexSearch[2]
+        msg = regexSearch[3]
+
+        # extraData = {"structured_data": {"severity": severity, "module": module}}
+        payload = json.dumps({"severity": severity, "module": module, "message": msg})
+
+        print(payload)
+
+        match severity:
+            case "emergency":
+                self.log.critical(payload)
+            case "alert":
+                self.log.critical(payload)
+            case "critical":
+                self.log.critical(payload)
+            case "error":
+                self.log.error(payload)
+            case "warning":
+                self.log.warning(payload)
+            case "notice":
+                self.log.info(payload)
+            case "informational":
+                self.log.info(payload)
+                # self.log.info(f"severity={severity} name={name} message={msg}")
+            case "debug":
+                self.log.debug(payload)
 
     def set_subscriptions(self):
         self.subscribe_user("console")
