00d7f1718edcd5214c7c74c5f51a48bc371d945a
Martin George <marty@dingopaws.net>
2026-01-27 16:21:38 +1100

    Fixed jobtype

--- screeps_exporter/memory.py
+++ screeps_exporter/memory.py
@@ -728,31 +728,31 @@ class Memory:
             print(f"create_screeps_profiler_method: {error}")
 
     def pollMetrics(self):
-        try:
-            memory = self.pollMemory()
+        # try:
+        memory = self.pollMemory()
 
-            self.roomMemory = memory["rooms"]
-            self.spawnMemory = memory["spawns"]
-            self.spawnHeldMemory = memory["spawnHeld"]
-            self.jobMemory = memory["jobs"]
-            self.creepMemory = memory["creeps"]
-            self.globalMemory = memory["global"]
-            self.profilerMemory = memory["profiler"]
+        self.roomMemory = memory["rooms"]
+        self.spawnMemory = memory["spawns"]
+        self.spawnHeldMemory = memory["spawnHeld"]
+        self.jobMemory = memory["jobs"]
+        self.creepMemory = memory["creeps"]
+        self.globalMemory = memory["global"]
+        self.profilerMemory = memory["profiler"]
 
-            self.pollCreepMetrics()
+        self.pollCreepMetrics()
 
-            self.pollGlobalGclMetrics()
-            self.pollGlobalCpuMetrics()
-            self.pollGlobalTimeMetrics()
+        self.pollGlobalGclMetrics()
+        self.pollGlobalCpuMetrics()
+        self.pollGlobalTimeMetrics()
 
-            self.pollRoomMetrics()
-            self.pollSpawnMetrics()
-            self.pollSpawnHeldMetrics()
-            self.pollJobMetrics()
-            if self.config["exporter"]["monitorProfiler"] == True:
-                self.pollProfilerMetrics()
-        except Exception as error:
-            print(f"pollMetrics: {error}")
+        self.pollRoomMetrics()
+        self.pollSpawnMetrics()
+        self.pollSpawnHeldMetrics()
+        self.pollJobMetrics()
+        if self.config["exporter"]["monitorProfiler"] == True:
+            self.pollProfilerMetrics()
+        # except Exception as error:
+        # print(f"pollMetrics: {error}")
 
         sleep(self.config["exporter"]["interval"])
 
@@ -1186,7 +1186,8 @@ class Memory:
             for statusName in jobStatusNames:
                 statusCounts[statusName] = 0
 
-            for jobData in self.jobMemory:
+            for jobName in self.jobMemory:
+                jobData = self.jobMemory[jobName]
                 typeCounts[jobData["type"]] = typeCounts[jobData["type"]] + 1
                 statusCounts[jobData["status"]] = statusCounts[jobData["status"]] + 1
 
